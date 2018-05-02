a6d86ee5cfc3f5b185f3cb6d84f488b4a338ec08
Volodymyr Yevtushenko <volodymyr_yevtushenko@epam.com>
2018-05-02 13:42:37 +0300

    Complete section hello

---
+++ hello_test.go
@@ -0,0 +1,36 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got '%s' want '%s'", got, want)
+		}
+	}
+
+	t.Run("say hello to people", func(t *testing.T) {
+		got := Hello("Bob", "")
+		want := "Hello, Bob"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say hello when an empty string is supplied", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Jack", "french")
+		want := "Bonjour, Jack"
+		assertCorrectMessage(t, got, want)
+	})
+}
