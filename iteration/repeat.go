6264c5bd48b51a48f3301abef89d6fd8db1177e2
Volodymyr Yevtushenko <volodymyr_yevtushenko@epam.com>
2018-05-02 16:17:16 +0300

    Add iterators

---
+++ iteration/repeat.go
@@ -0,0 +1,10 @@
+package iteration
+
+func Repeat(char string, count int) string {
+	var repeated string
+	for i := 1; i <= count; i++ {
+		repeated += char
+	}
+
+	return repeated
+}
---
+++ iteration/repeat_test.go
@@ -0,0 +1,27 @@
+package iteration
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRepeat(t *testing.T) {
+	repeated := Repeat("a", 3)
+	expected := "aaa"
+
+	if repeated != expected {
+		t.Errorf("expected %s but got %s", expected, repeated)
+	}
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 3)
+	}
+}
+
+func ExampleRepeat() {
+	repeat := Repeat("a", 2)
+	fmt.Println(repeat)
+	// Output: aa
+}
