48f17dfacd34168d011b689260d605305a2e566c
Volodymyr Yevtushenko <volodymyr_yevtushenko@epam.com>
2018-05-02 16:26:10 +0300

    Add helloString as package

--- main.go
+++ main.go
@@ -1,34 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
-
-const englishPrefix = "Hello, "
-const spanishHelloPrefix = "Hola, "
-const frenchHelloPrefix = "Bonjour, "
-
-func Hello(name string, language string) string {
-	if name == "" {
-		name = "World"
-	}
-	return greetingPrefix(strings.ToLower(language)) + name
-}
-
-func greetingPrefix(language string) (prefix string) {
-	switch language {
-	case "french":
-		prefix = frenchHelloPrefix
-	case "spanish":
-		prefix = spanishHelloPrefix
-	default:
-		prefix = englishPrefix
-	}
-
-	return prefix
-}
+import "fmt"
 
 func main() {
-	fmt.Println(Hello("Bob", "spanish"))
+	fmt.Println("Hello")
 }
